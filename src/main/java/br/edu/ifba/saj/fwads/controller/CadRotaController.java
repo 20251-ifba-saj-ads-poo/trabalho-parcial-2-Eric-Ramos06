89dd100c7e3c3d57f077a91de4c5bde73fb4ff43
Eric-Ramos06 <ericasr2006@gmail.com>
2025-08-22 23:37:32 -0300

    Att rota

--- src/main/java/br/edu/ifba/saj/fwads/controller/CadRotaController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/CadRotaController.java
@@ -1,6 +1,39 @@
 package br.edu.ifba.saj.fwads.controller;
 
+import br.edu.ifba.saj.fwads.Dados;
+import br.edu.ifba.saj.fwads.model.Rota;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class CadRotaController {
-    
-    
+
+    @FXML
+    private TextField txOrigem;
+
+    @FXML
+    private TextField txDestino;
+
+    @FXML
+    private Label lbOrigem;
+
+    @FXML
+    private Label lbDestino;
+
+    @FXML
+    private void salvarRota() {
+        Rota novaRota = new Rota(txOrigem.getText(), txDestino.getText());
+        new Alert(AlertType.INFORMATION,
+                "Cadastrando Rota:" + novaRota.getOrigem() + " - " + novaRota.getDestino()).showAndWait();
+        Dados.listaRotas.add(novaRota);
+        limparTela();
+    }
+    @FXML
+    private void limparTela() {
+        txOrigem.setText("");
+        txDestino.setText("");
+    }
+
 }
